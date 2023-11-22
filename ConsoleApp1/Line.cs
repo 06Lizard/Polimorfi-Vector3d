55bd386b021190910afdb6c42181fb0dc661e7fb
Walter Wänerskog <walter.wanerskog2@gmail.com>
2023-11-22 11:49:06 +0100

    Added Line classs
    Vector3d => Point3d => Line3d

---
+++ ConsoleApp1/Line.cs
@@ -0,0 +1,69 @@
+﻿class Line
+{
+    Point3d p1;
+    Point3d p2;
+
+    double Length { get {  return GetLength(); } }
+
+    public Line(Point3d p1, Point3d p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    // Linjär interpolation
+    // Linerar interpolation
+    Point3d Lerp(double t) // Standard name
+    {
+        // Check so that t is between 0 & 1;
+        t = Math.Max(0, Math.Min(1, t));
+        // Calckulate point
+        double newX = p1.X + t * (p2.X - p1.X);
+        double newY = p1.Y + t * (p2.Y - p1.Y);
+        double newZ = p1.Z + t * (p2.Z - p1.Z);
+        
+        return new Point3d(newX, newY, newZ);
+    }
+
+    public void Draw()
+    {
+        for (double t = 0; t <= 1; t+=0.01)
+        {
+            Point3d interpolatedPoint = Lerp(t);
+            double consoleX = (double)interpolatedPoint.X;
+            double consoleY = (double) interpolatedPoint.Y;
+            double consoleZ = (double)interpolatedPoint.Z;
+            Console.SetCursorPosition((int)consoleX, (int)consoleY);
+            Console.ForegroundColor = AntiAliasing(consoleX, consoleY);
+            Console.Write('#');
+            Console.ResetColor();
+        }
+    }
+
+    ConsoleColor AntiAliasing(double ConsoleX, double ConsoleY)
+    {
+        /*if () // code that checks how close to centre of a pixel pos ConsoleX, ConsoleY is
+        {
+            return ConsoleColor.Gray;
+        }
+        else if ()
+        {
+            return ConsoleColor.DarkGray;
+        }*/
+        return ConsoleColor.White;
+    }
+
+    public double GetLength()
+    {
+        double deltaX = p1.X + p2.X;    //Math.Abs(p1.X + p2.X); for abs delta
+        double deltaY = Math.Abs(p1.Y + p2.Y);
+        double deltaZ = Math.Abs(p1.Z + p2.Z);
+
+        return Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+    }
+
+    public override string ToString()
+    {
+        return $"{p1} -- {p2}";
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,33 +2,10 @@
 {
     static void Main()
     {
-        Vector3d v1 = new Vector3d(1, 2, 5); // Ver 1
-        Vector3d v0 = new Vector3d(-1, 3, 2); // Ver0
-        Console.WriteLine(v1);
-        Vector3d v2 = new Vector3d(v1); // Ver2
-        Console.WriteLine(v2);
-        Console.WriteLine(v1.Length);
-        Vector3d v3 = v1 + v2;
-        Console.WriteLine(v3);
-        Vector3d v4 = v1 + v3;
-        Console.WriteLine(v4);
-        Console.WriteLine(v3[1]);
-
-        v0[2] = 4;
-        Console.WriteLine(v0[2]);
-        double dot = Vector3d.Dot(v1, v0);
-        Console.WriteLine(dot);
-
-        double dotProduct = v1 * v0;
-        Console.WriteLine(dotProduct);
-        object obj = new();
-        obj.ToString();
-
-        Point3d p1 = new(3, 2, 8);
-        Point3d p2 = new(p1, v2);
-        Point3d p3 = Point3d.Add(p1, p2);
-        Point3d p4 = p1 + p2;
-        Point3d p5 = p1 + v3;
-        Console.WriteLine(p4[7]);
+        Point3d p1 = new(10, 2, 3);
+        Point3d p2 = new(25, 29, -3);
+        Line l1 = new(p1, p2);
+        Console.WriteLine(l1);
+        l1.Draw();
     }
 }
